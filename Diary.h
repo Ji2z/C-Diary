895c75048f9b5133ef36bd66e19a4a74d8796f30
Ji2z <39006950+Ji2z@users.noreply.github.com>
2018-06-22 23:34:59 +0900

    Update Diary.h

--- Diary.h
+++ Diary.h
@@ -1,4 +1,4 @@
-
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <direct.h>
@@ -8,25 +8,7 @@ using namespace std;
 
 class Date {
 public:
- char year[10];
- char month[10];
- char day[10];
-};
-
-class Emotion {
-public:
- inline void GetEmotion(char em) {
-  char em1;
-  if ((em != 'H') && (em != 'J') && (em != 'S') && (em != 'A') && (em != 'B')) {
-   cout << "다시 입력하세요.";
-   cin >> em1;
-   GetEmotion(em1);
-  }
- }
-};
-
-class Diary {
-public:
- Emotion emotion;
- Date date;
+	char year[10];
+	char month[10];
+	char day[10];
 };
