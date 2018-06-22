f73488b3ce8654e9914db05e35103f22f19909c6
Ji2z <39006950+Ji2z@users.noreply.github.com>
2018-06-22 05:03:43 +0000

    Diary header

--- Diary.h
+++ Diary.h
@@ -1,4 +1,4 @@
-#include <iostream>
+
 #include <string>
 #include <fstream>
 #include <direct.h>
@@ -8,40 +8,25 @@ using namespace std;
 
 class Date {
 public:
-	char year[10];
-	char month[10];
-	char day[10];
+ char year[10];
+ char month[10];
+ char day[10];
 };
 
 class Emotion {
-	int happy{ happy = 0 };
-	int joyth{ joyth = 0 };
-	int sad{ sad = 0 };
-	int angry{ angry = 0 };
-	int boring{ boring = 0 };
 public:
-	inline void GetEmotion(char em) {
-		char em1;
-		if (em == 'H')
-			happy++;
-		else if (em == 'J')
-			joyth++;
-		else if (em == 'S')
-			sad++;
-		else if (em == 'A')
-			angry++;
-		else if (em == 'B')
-			boring++;
-		else {
-			cout << "다시 입력하세요.";
-			cin >> em1;
-			GetEmotion(em1);
-		}
-	};
+ inline void GetEmotion(char em) {
+  char em1;
+  if ((em != 'H') && (em != 'J') && (em != 'S') && (em != 'A') && (em != 'B')) {
+   cout << "다시 입력하세요.";
+   cin >> em1;
+   GetEmotion(em1);
+  }
+ }
 };
 
 class Diary {
 public:
-	Emotion emotion;
-	Date date;
+ Emotion emotion;
+ Date date;
 };
