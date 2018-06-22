289158386f680891f1f3499afcfe87be64364fc9
Ji2z <39006950+Ji2z@users.noreply.github.com>
2018-06-22 23:34:26 +0900

    Write a diary

--- GetDiary.cpp
+++ GetDiary.cpp
@@ -4,23 +4,18 @@
 
 
 void GetDairy() {
-	char wh, em;
-	char letter[500];
-	char filename[20];
+	string wh, em;
+	char letter[10000];
 	Date date;
-	Emotion emotion;
 
 	cout << "날짜(YYYY MM DD): ";
 	cin >> date.year >> date.month >> date.day;
 	cout << "날씨: ";
 	cin >> wh;
-	cout << "감정(기쁨:H, 설렘:J, 슬픔:S, 화남:A, 무료:B): ";
+	cout << "기분: ";
 	cin >> em;
-	emotion.GetEmotion(em);
-
-	cout << "일기를 입력하세요." << endl;
-	cin.getline(letter, 500, '끝');
+	cout << endl << "일기를 입력하세요." << endl;
+	cin.getline(letter, 10000, '끝');
 
 	WriteTxt(date.year, date.month, date.day, letter);
-	
 }
