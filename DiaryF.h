f908927d6dffabf0dcfa8839145f7b92d287d963
Ji2z <39006950+Ji2z@users.noreply.github.com>
2018-06-22 23:35:34 +0900

    Fuctions

--- DiaryF.h
+++ DiaryF.h
@@ -3,9 +3,19 @@
 #include <fstream>
 #include <direct.h>
 #include <stdlib.h>
+#include<iomanip>
+#include<conio.h>
+#include<time.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <conio.h>
 
 
 using namespace std;
 
 void GetDairy();
 void WriteTxt(string name1, string name2, string name3, string text);
+void PrintDiary();
+void RandomQuest();
