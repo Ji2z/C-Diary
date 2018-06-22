ced7106d90d914a9304e176847455acc32c4556f
Ji2z <39006950+Ji2z@users.noreply.github.com>
2018-06-22 04:21:29 +0000

    Delete MakeDir.cpp

--- MakeDir.cpp
+++
@@ -1,9 +0,0 @@
-#include "Diary.h"
-#include "DiaryF.h"
-
-void MakeDir(char filename[20], char *year, char* month) {// Æú´õ»ı¼º(ex.2018.06)
-	strcat(filename, year);
-	strcat(filename, ".");
-	strcat(filename, month);
-	_mkdir(filename);
-}
